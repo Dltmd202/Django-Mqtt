1f381b1ff8363f7c7bba939d401aa83c6dba39f3
Dltmd202 <dltmd202@gmail.com>
2021-11-26 05:05:09 +0900

    multiprocess safe terminate

--- pi_client/main.py
+++ pi_client/main.py
@@ -11,10 +11,19 @@ class Client:
         self.dist = Distance(ip)
         self.temp = Temp_Hum(ip)
         self.sensors = [self.pir, self.dist, self.temp]
+        self.processingQ = []
 
     def run(self):
         for sensor in self.sensors:
             p = multiprocessing.Process(target=sensor.run())
+            self.processingQ.append(p)
+
+        try:
+            for process in self.processingQ:
+                process.join()
+        except KeyboardInterrupt:
+            for process in self.processingQ:
+                process.terminate()
 
 
 if __name__ == "__main__":

872ad658622b749a25542bb20d371d4a900d1800
Dltmd202 <dltmd202@gmail.com>
2021-12-03 14:15:06 +0900

    home view

--- mqttApp/views.py
+++ mqttApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from mqttApp.models import Information
+from mqttApp.serializers import InformationSerializer
 # Create your views here.
+
+
+class InformationDetail(generics.RetrieveUpdateAPIView):
+    queryset = Information
+    serializer_class = InformationSerializer

d03a151de304d44408a041b46f90bd7c1791eec2
Dltmd202 <dltmd202@gmail.com>
2021-12-07 02:58:20 +0900

    host setting

--- smart_window/settings.py
+++ smart_window/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%d$*rxp4i@1=353yxw&(-mrhryfq_87=^n=q42$l(4ztcfi)1b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['192.168.0.35', '192.168.0.16']
 
 
 # Application definition

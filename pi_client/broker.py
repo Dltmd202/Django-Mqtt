684ce732db30b23938501885c3dd9a2220335285
SongSiWoon <siwoons7319@gmail.com>
2021-12-12 04:04:28 +0900

    server

--- pi_client/broker.py
+++ pi_client/broker.py
@@ -46,10 +46,11 @@ class ServerApplication:
             data = self.get_data()
             print(f"publising {data}")
             res = requests.patch(
-                "http://172.20.10.7:8000/window/inf/1/",
+                "http://172.20.10.7:8000/window/inf/1",
                 headers=headers,
                 data=json.dumps(data)
             )
+            print(res.text)
 
         client.on_connect = on_connect
         client.on_message = on_message

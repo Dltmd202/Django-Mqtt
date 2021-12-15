79a07592a192ed6be92901e4a7a6e885f9ce80a5
SongSiWoon <siwoons7319@gmail.com>
2021-12-16 00:06:23 +0900

    broker

--- pi_client/broker.py
+++ pi_client/broker.py
@@ -109,14 +109,10 @@ class ServerApplication:
             "is_open": False,
             "is_lock": False
         }
-        if self.distance > 10 and self.is_open:
-            res["is_open"] = True
-            res["is_lock"] = False
-            return res
-        if self.distance <= 10 and not self.is_open:
-            res["is_open"] = False
-            res["is_lock"] = False
-            return res
+        if self.distance > 10 and not self.is_open:
+            self.is_open = True
+        if self.distance <= 10 and self.is_open:
+            self.is_open = False
         if self.is_person:
             res["is_open"] = False
             res["is_lock"] = True

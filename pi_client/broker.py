0247935ff8dd433bc4742f12454d653b4bd6da50
SongSiWoon <siwoons7319@gmail.com>
2021-12-18 21:52:33 +0900

    server

--- pi_client/broker.py
+++ pi_client/broker.py
@@ -145,7 +145,7 @@ class ServerApplication:
                 res["is_lock"] = False
             self.open_order = None
             return res
-        if self.rain and self.rain > 40:
+        if self.rain:
             print("우천으로 인해 닫는 중$$$$$$$$$$$$$$")
             res["is_open"] = False
             res["is_lock"] = False
